856897eff59f4744d26df41e78653a7a04cd1fdc
Samuel Karlsson <samuelkarlsson23@gmail.com>
2018-09-28 11:10:14 +0200

    hej

---
+++ Index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+
+<title>Tvattstuga</title>
+
+</head>
+
+
+
+
+
+<body>
+
+<form action="Index.php">
+
+  <div class="imgcontainer">
+    <img src="avatar.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Användarnamn</b></label>
+    <input type="text" placeholder="Skriv in Användarnamn" name="uname" required>
+
+    <label for="psw"><b>Lösenord</b></label>
+    <input type="password" placeholder="Skriv in Lösenord" name="psw" required>
+
+    <button type="submit">Login</button>
+    
+  </div>
+
+</form>
+
+</body>
+
+
+
+
+</html>
