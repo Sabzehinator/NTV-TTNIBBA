f01899f9306b5cea2854889f6b3d4c25a1ed78c2
Tommie55555 <tommie.van.klaveren16@itgksd.se>
2018-10-03 16:15:37 +0200

    Lade till något viktigt
    Jätteviktigt.

---
+++ Faktiskt-Cancer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+
+	<title>CANCERSTUGA</title>
+
+</head>
+
+
+
+
+
+<body>
+
+	<form action="Index.php">
+
+		<div class="imgcontainer">
+			<image src="https://i.ytimg.com/vi/k8XXcz08hPA/hqdefault.jpg"/>
+		</div
+
+	</form>
+
+	</body>
+
+
+
+
+</html>
