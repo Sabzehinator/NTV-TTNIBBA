395efa3b180e4296d6cf0e00f46747d764e38bfe
Simon <simon.larsell16@itgksd.se>
2018-10-12 10:57:00 +0200

    bs login
    login fick bootstrap

--- login.php
+++ login.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
 <html lang = "sv">
+
 <head>
 <title> Login Sida</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
 <body>
-
-<div id="frm">
-
-<form action="process.php" method="POST">
-
-<p>
-<label>Lägenhetsnummer:</label>
-<input type="text" id="username" name="username" />
-</p>
-<p>
-<label>Lösenord:</label>
-<input type="password" id="pass" name="password" />
-</p>
-<p>
-<input type="submit" id="btn" value="Login" />
-</p>
-
-
-
-
-
-
-</form>
-
-</div>
-
-
+    <div action="process.php" id="login" method="POST">
+        <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-info">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="password" name="password" id="pass" class="form-control">
+                            </div>
+                            <div class="form-group">        
+                                <input type="submit" name="login" class="btn btn-info btn-md" value="Login">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
 
 <?php
