7058ab34b0247c039e0a3f5c14b969b33336b03f
Simon <simon.larsell16@itgksd.se>
2018-10-12 11:20:49 +0200

    jio
    dfdf

--- login.php
+++ login.php
@@ -6,9 +6,9 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body>
+<body <body style="background-image: url(https://www.centralabygg.com/siteassets/regional-assets/emea/centrala-bygg-sweden/buying-guides/tvattmaskiner/washingmachines-electrolux-side-by-side-2400x1800.jpg)">
     <div action="process.php" id="login" method="POST">
-        <h3 class="text-center text-white pt-5">Login form</h3>
+        <h3 class="text-body text-center text-white pt-5"></h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
