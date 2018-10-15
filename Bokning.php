9ba9f8ec4821275fb21e9aa391893442022f736b
Simon <simon.larsell16@itgksd.se>
2018-10-15 11:00:41 +0200

    Update Bokning.php

--- Bokning.php
+++ Bokning.php
@@ -1,21 +1,112 @@
 <!DOCTYPE html>
-<html lang="sv">
+<html lang = "sv">
+
 <head>
-
-<title>Tvattstuga</title>
-
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<title>Bokning</title>
 </head>
-
-
-<body>
-
-
-
-
+<body <body style="background-image: url(https://www.centralabygg.com/siteassets/regional-assets/emea/centrala-bygg-sweden/buying-guides/tvattmaskiner/washingmachines-electrolux-side-by-side-2400x1800.jpg)">
+ <div class="container">
+<form>  
+  <table class="table table-bordered table-dark">
+    <thead>
+      <tr>
+        <th>Måndag</th>
+        <th>Tisdag</th>
+        <th>Onsdag</th>
+		<th>Torsdag</th>
+        <th>Fredag</th>
+        <th>Lördag</th>
+		<th>Söndag</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>08:00 - 10:00</td>
+        <td>08:00 - 10:00</td>
+        <td>08:00 - 10:00</td>
+		<td>08:00 - 10:00</td>
+        <td>08:00 - 10:00</td>
+        <td>08:00 - 10:00</td>
+		<td>08:00 - 10:00</td>
+      </tr>
+      <tr>
+        <td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+		<td>10:00 - 12:00</td>
+      </tr>
+      <tr>
+        <td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+		<td>12:00 - 14:00</td>
+      </tr>
+	  <tr>
+        <td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+		<td>14:00 - 16:00</td>
+      </tr>
+	  <tr>
+        <td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+		<td>16:00 - 18:00</td>
+      </tr>
+	  <tr>
+        <td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+		<td>18:00 - 20:00</td>
+      </tr>
+	  <tr>
+        <td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+		<td>20:00 - 22:00</td>
+      </tr>
+	  <tr>
+        <td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+		<td>22:00 - 00:00</td>
+      </tr>
+    </tbody>
+  </table>
+  </form>
+</div>
 </body>
 
-
-
-
-
 </html>
+
+<?php
+
+
+?>
+
+
+
+
