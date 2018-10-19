074dcf009a43e0317399fea7daf8df01b4bd28ee
Simon <simon.larsell16@itgksd.se>
2018-10-19 10:43:13 +0200

    nice

--- Bokning.php
+++ Bokning.php
@@ -2,6 +2,124 @@
 <html lang = "sv">
 
 <head>
+<title> Login Sida</title>
+<link rel="stylesheet" type="text/css" href="stylebokning.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body <body style="background-image: url(https://www.centralabygg.com/siteassets/regional-assets/emea/centrala-bygg-sweden/buying-guides/tvattmaskiner/washingmachines-electrolux-side-by-side-2400x1800.jpg)">
+    <form action="process.php" id="login" method="POST">
+        <h3 class="text-body text-center text-white pt-5"></h3>
+        <div class="container">
+            <div id="login-row" class="row">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+						<table class="table table-bordered">
+							<thead>
+							  <tr>
+								<th>Måndag</th>
+								<th>Tisdag</th>
+								<th>Onsdag</th>
+								<th>Torsdag</th>
+								<th>Fredag</th>
+								<th>Lördag</th>
+								<th>Söndag</th>
+							  </tr>
+							</thead> 
+						<tbody>
+							  <tr>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+								<td><button type="button" class="btn btn-info active">08:00 - 10:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+								<td><button type="button" class="btn btn-primary active">10:00 - 12:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+								<td><button type="button" class="btn btn-info active">12:00 - 14:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+								<td><button type="button" class="btn btn-primary active">14:00 - 16:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+								<td><button type="button" class="btn btn-info active">16:00 - 18:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+								<td><button type="button" class="btn btn-primary active">18:00 - 20:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+								<td><button type="button" class="btn btn-info active">20:00 - 22:00</button></td>
+							  </tr>
+							  <tr>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+								<td><button type="button" class="btn btn-primary active">22:00 - 00:00</button></td>
+							  </tr>
+							</tbody>
+						  </table>
+						  <input type="submit" name="boka" class="btn btn-info btn-md" value="Boka">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
+
+<?php
+
+
+?>
+<!-- <!DOCTYPE html>
+<html lang = "sv">
+
+<head>
 <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <title>Bokning</title>
@@ -131,6 +249,6 @@
 
 ?>
 
-
+-->
 
 
