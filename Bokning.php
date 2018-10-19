09a27c912819602d5ffad877aba424612dfa46c8
Samuel Karlsson <samuelkarlsson23@gmail.com>
2018-10-19 11:03:57 +0200

    123

--- Bokning.php
+++ Bokning.php
@@ -4,7 +4,6 @@
 <head>
 <title> Login Sida</title>
 <link rel="stylesheet" type="text/css" href="stylebokning.css">
-<script src ="bokningJS.js"</script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body <body style="background-image: url(https://www.centralabygg.com/siteassets/regional-assets/emea/centrala-bygg-sweden/buying-guides/tvattmaskiner/washingmachines-electrolux-side-by-side-2400x1800.jpg)">
